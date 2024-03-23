d0f16769ac791986ade6d6ab47dbda0e970a4665
tonymk0987 <k.tony.muriuki@gmail.com>
2024-03-23 20:57:09 +0300

    Completed lab

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  function returnsANamedFunction() {
+    return function namedFunction() {
+
+    };
+  }
+function returnsAnAnonymousFunction() {
+    return function() {
+      
+    };
+  }
+  
